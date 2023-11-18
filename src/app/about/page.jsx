134568a4db76a6ac94edff0a707361791f843356
shovon231 <shovon231@gmail.com>
2023-11-18 02:42:08 -0500

    adding the project to github

---
+++ src/app/about/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./page.module.css";
+
+const About = () => {
+  return <div className={styles.container}>About</div>;
+};
+
+export default About;
---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./page.module.css";
+
+const Dasshboard = () => {
+  return <div className={styles.container}>Dasshboard</div>;
+};
+
+export default Dasshboard;
